46d6d7b7f9fcedb061f80978964b77f09efd087a
Gulyas Mate <mategulyas01@gmail.com>
2023-12-10 20:20:55 +0100

    searchbar added, and styled.

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,76 +1,92 @@
+import * as Icon from "react-native-feather";
 import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { themeColors } from "../theme";
 
 function HomeScreen() {
   return (
-    <View>
-      <Text>This is home screen.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={styles.searchBarContainer} />
+      <View style={styles.searchBarContainer}>
+        <View style={styles.searchInputContainer}>
+          <Icon.Search height="25" stroke="gray" />
+          <TextInput placeholder="Restaurants" style={styles.searchInput} />
+          <View style={styles.locationContainer}>
+            <Icon.MapPin height="20" width="20" stroke="gray" />
+            <Text style={styles.locationText}>Budapest</Text>
+          </View>
+        </View>
+
+        <View style={styles.slidersContainer}>
+          <Icon.Sliders height="20" strokeWidth={2.5} stroke="white" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 export default HomeScreen;
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "white",
-//   },
-//   searchBarContainer: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     paddingHorizontal: 16,
-//     paddingBottom: 8,
-//   },
-//   searchInputContainer: {
-//     flexDirection: "row",
-//     flex: 1,
-//     alignItems: "center",
-//     padding: 12,
-//     borderRadius: 999,
-//     borderWidth: 1,
-//     borderColor: "gray",
-//   },
-//   searchIcon: {
-//     height: 25,
-//     width: 25,
-//     stroke: "gray",
-//   },
-//   searchInput: {
-//     marginLeft: 8,
-//     flex: 1,
-//   },
-//   locationContainer: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     borderWidth: 0,
-//     borderLeftWidth: 2,
-//     paddingLeft: 8,
-//     borderColor: "gray",
-//   },
-//   locationIcon: {
-//     height: 20,
-//     width: 20,
-//     stroke: "gray",
-//   },
-//   locationText: {
-//     color: "gray",
-//   },
-//   slidersContainer: {
-//     backgroundColor: themeColors.bgColor(1),
-//     padding: 12,
-//     borderRadius: 999,
-//   },
-//   slidersIcon: {
-//     height: 20,
-//     width: 20,
-//     strokeWidth: 2.5,
-//     stroke: "white",
-//   },
-//   mainContent: {
-//     marginTop: 5,
-//     paddingBottom: 50,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  searchBarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    paddingBottom: 8,
+  },
+  searchInputContainer: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center",
+    padding: 12,
+    borderRadius: 999,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+  searchIcon: {
+    height: 25,
+    width: 25,
+    stroke: "gray",
+  },
+  searchInput: {
+    marginLeft: 8,
+    flex: 1,
+  },
+  locationContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 0,
+    borderLeftWidth: 2,
+    paddingLeft: 8,
+    borderColor: "gray",
+  },
+  // locationIcon: {
+  //   height: 20,
+  //   width: 20,
+  //   stroke: "gray",
+  // },
+  locationText: {
+    color: "gray",
+  },
+  slidersContainer: {
+    backgroundColor: themeColors.bgColor(1),
+    padding: 12,
+    borderRadius: 999,
+  },
+  slidersIcon: {
+    height: 20,
+    width: 20,
+    strokeWidth: 2.5,
+    stroke: "white",
+  },
+  mainContent: {
+    marginTop: 5,
+    paddingBottom: 50,
+  },
+});
