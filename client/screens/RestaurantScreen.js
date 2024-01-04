e11441b82997802d82c614f8dfdb1a45d1bcceb6
Gulyas Mate <mategulyas01@gmail.com>
2024-01-04 18:58:34 +0100

    The Image now is visible in RestaurantScreen.

--- client/screens/RestaurantScreen.js
+++ client/screens/RestaurantScreen.js
@@ -1,27 +1,99 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
-import { useNavigation, useRoute } from "@react-navigation/native";
-import { Image } from "react-native-feather";
+import { useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RestaurantScreen() {
   const { params } = useRoute();
-  let item = params;
-  // const navigation = useNavigation();
-  console.log(item);
+  const item = params;
+
   return (
-    <SafeAreaView>
-      <ScrollView style={{ position: "relative" }}>
-        <Image style={{ width: 100, height: "72px" }} source={item.image} />
-        <Text>This will be the Restaurant Screen.</Text>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image style={styles.image} source={item.image} />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = {
-  searchInput: {
+  container: {
+    position: "relative",
+    paddingBottom: 36,
+  },
+  image: {
     width: "100%",
-    height: "72px",
+    height: 270,
+  },
+  backButton: {
+    position: "absolute",
+    top: 14,
+    left: 4,
+    backgroundColor: "gray",
+    padding: 2,
+    borderRadius: 999,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+  },
+  infoContainer: {
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    backgroundColor: "white",
+    marginTop: -12,
+    paddingTop: 6,
+    paddingHorizontal: 5,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  ratingContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 1,
+  },
+  ratingIconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  ratingIcon: {
+    height: 16,
+    width: 16,
+  },
+  ratingText: {
+    fontSize: 12,
+    color: "#4B5563",
+    paddingLeft: 4,
+  },
+  greenText: {
+    color: "green",
+  },
+  grayText: {
+    color: "gray",
+  },
+  boldGrayText: {
+    fontWeight: "bold",
+    color: "gray",
+  },
+  addressContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  description: {
+    color: "#4B5563",
+    marginTop: 2,
+  },
+  menuContainer: {
+    paddingBottom: 36,
+    backgroundColor: "white",
+  },
+  menuTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingHorizontal: 4,
+    paddingVertical: 4,
   },
 };
