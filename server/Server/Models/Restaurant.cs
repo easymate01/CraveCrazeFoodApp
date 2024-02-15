36a9d5ce1a2418819178576209e9410f4727badd
Gulyas Mate <mategulyas01@gmail.com>
2024-02-15 21:35:09 +0100

    Backend: Dish and Restraurant models added.

---
+++ server/Server/Models/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿namespace Server.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public double Lng { get; set; }
+        public double Lat { get; set; }
+        public string Address { get; set; }
+        public int Stars { get; set; }
+        public string Reviews { get; set; }
+        public string Category { get; set; }
+    }
+}
