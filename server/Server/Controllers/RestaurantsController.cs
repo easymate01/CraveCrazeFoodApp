5d234874455666db267f7bca27a4ceb971d186f3
Gulyas Mate <mategulyas01@gmail.com>
2024-02-17 14:36:58 +0100

    Restaurant Controller added.

---
+++ server/Server/Controllers/RestaurantsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Services;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantsController : Controller
+    {
+        private readonly IRestaurant _restaurantService;
+        private readonly ILogger<RestaurantsController> _logger;
+
+        public RestaurantsController(IRestaurant restaurantService, ILogger<RestaurantsController> logger)
+        {
+            _restaurantService = restaurantService;
+            _logger = logger;
+        }
+    }
+}
+
