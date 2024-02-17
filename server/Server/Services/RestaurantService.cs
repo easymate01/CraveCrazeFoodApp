0741af89802d570bad61ee47cc08db7d14a4f755
Gulyas Mate <mategulyas01@gmail.com>
2024-02-17 15:04:27 +0100

    RestaurantService: CRUD added to service layer.

--- server/Server/Services/RestaurantService.cs
+++ server/Server/Services/RestaurantService.cs
@@ -1,4 +1,5 @@
-﻿using Server.DTOs;
+﻿using Microsoft.EntityFrameworkCore;
+using Server.DTOs;
 using Server.Models;
 
 namespace Server.Services
@@ -12,23 +13,68 @@ namespace Server.Services
             _dbContext = dbContext;
         }
 
-        public async Task<Restaurant> CreateAsync(RestaurantDto restaurant)
+        public async Task<List<Restaurant>> GetAllAsync()
+        {
+            return await _dbContext.Restaurants.ToListAsync();
+        }
+
+        public async Task<Restaurant> GetByIdAsync(int id)
+        {
+            return await _dbContext.Restaurants.FindAsync(id);
+        }
+
+        public async Task<Restaurant> CreateAsync(RestaurantDto restaurantDto)
         {
             var newRestaurant = new Restaurant
             {
-                Name = restaurant.Name,
-                Image = restaurant.Image,
-                Description = restaurant.Description,
-                Lng = restaurant.Lng,
-                Lat = restaurant.Lat,
-                Address = restaurant.Address,
-                Stars = restaurant.Stars,
-                Reviews = restaurant.Reviews,
-                Category = restaurant.Category
+                Name = restaurantDto.Name,
+                Image = restaurantDto.Image,
+                Description = restaurantDto.Description,
+                Lng = restaurantDto.Lng,
+                Lat = restaurantDto.Lat,
+                Address = restaurantDto.Address,
+                Stars = restaurantDto.Stars,
+                Reviews = restaurantDto.Reviews,
+                Category = restaurantDto.Category
             };
             _dbContext.Restaurants.Add(newRestaurant);
             await _dbContext.SaveChangesAsync();
             return newRestaurant;
+        }
+
+        public async Task<bool> UpdateAsync(int id, RestaurantDto restaurantDto)
+        {
+            var restaurantToUpdate = await _dbContext.Restaurants.FindAsync(id);
+            if (restaurantToUpdate == null)
+            {
+                return false;
+            }
+
+            restaurantToUpdate.Name = restaurantDto.Name;
+            restaurantToUpdate.Image = restaurantDto.Image;
+            restaurantToUpdate.Description = restaurantDto.Description;
+            restaurantToUpdate.Lng = restaurantDto.Lng;
+            restaurantToUpdate.Lat = restaurantDto.Lat;
+            restaurantToUpdate.Address = restaurantDto.Address;
+            restaurantToUpdate.Stars = restaurantDto.Stars;
+            restaurantToUpdate.Reviews = restaurantDto.Reviews;
+            restaurantToUpdate.Category = restaurantDto.Category;
+
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var restaurantToDelete = await _dbContext.Restaurants.FindAsync(id);
+            if (restaurantToDelete == null)
+            {
+                return false;
+            }
+
+            _dbContext.Restaurants.Remove(restaurantToDelete);
+            await _dbContext.SaveChangesAsync();
+            return true;
         }
     }
 }
