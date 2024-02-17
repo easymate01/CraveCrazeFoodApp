9bffd611aeeef820069cc585de1d1c46302dbfba
Gulyas Mate <mategulyas01@gmail.com>
2024-02-17 15:09:37 +0100

    DishDto and IDish service interface added.

---
+++ server/Server/DTOs/DishDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Server.DTOs
+{
+    public class DishDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ server/Server/Services/IDish.cs
@@ -0,0 +1,14 @@
+﻿using Server.DTOs;
+using Server.Models;
+
+namespace Server.Services
+{
+    public interface IDish
+    {
+        Task<List<Dish>> GetAllAsync();
+        Task<Dish> GetByIdAsync(int id);
+        Task<Dish> CreateAsync(DishDto dishDto);
+        Task<bool> UpdateAsync(int id, DishDto dishDto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
