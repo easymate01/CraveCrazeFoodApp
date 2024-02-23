765292d79c4ead571c479a16cf92dad1bca4c265
Gulyas Mate <mategulyas01@gmail.com>
2024-02-23 14:34:17 +0100

    S3 models added.

---
+++ server/Server/DTOs/S3ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.DTOs
+{
+    public class S3ResponseDto
+    {
+        public int StatusCode { get; set; } = 200;
+        public string Message { get; set; } = "";
+
+    }
+}
---
+++ server/Server/Models/S3/AwsCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Models.S3
+{
+    public class AwsCredentials
+    {
+        public string AwsKey { get; set; } = "";
+        public string AwsSecret { get; set; } = "";
+    }
+}
