a52380e0bdf5d7c89bd2f7688d34160e87bb02e0
Gulyas Mate <mategulyas01@gmail.com>
2024-02-23 15:29:54 +0100

    CategoryController: UploadFile async task created.

---
+++ server/Server/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Models.S3;
+using Server.Services.AwsS3;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly IStorageService _storageService;
+        private readonly ILogger<CategoryController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public CategoryController(IStorageService storageServiceservice, ILogger<CategoryController> logger, IConfiguration configuration)
+        {
+            _storageService = storageServiceservice;
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpPost(Name = "UploadFile")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            await using var memoryStr = new MemoryStream();
+
+            await file.CopyToAsync(memoryStr);
+
+            var fileExt = Path.GetExtension(file.Name);
+            var objName = $"{Guid.NewGuid()}.{fileExt}";
+
+            var s3Obj = new S3Object()
+            {
+                BucketName = "cravecraze",
+                InputStream = memoryStr,
+                Name = objName
+            };
+
+            var cred = new AwsCredentials()
+            {
+                AwsKey = _configuration["AWSConfiguration:AWSAccessKey"],
+                AwsSecret = _configuration["AWSConfiguration:AWSSecretKey"],
+
+            };
+
+            var result = await _storageService.UploadFileAsync(s3Obj, cred);
+
+            return Ok(result);
+        }
+    }
+}
