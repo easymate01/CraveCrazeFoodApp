294cc7aee86ac98f45ad4da37764626bebe9c0a6
Gulyas Mate <mategulyas01@gmail.com>
2024-03-08 09:54:25 +0100

    StorageService: now working well.

--- server/Server/Services/AwsS3/StorageService.cs
+++ server/Server/Services/AwsS3/StorageService.cs
@@ -1,7 +1,45 @@
-﻿namespace Server.Services.AwsS3
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Server.Models.S3;
+
+namespace Server.Services.AwsS3
 {
     public class StorageService : IStorageService
     {
+        public async Task<string> UploadImageAsync(IFormFile image, AwsCredentials awsCredentials, string bucketName)
+        {
+            var credentials = new BasicAWSCredentials(awsCredentials.AwsKey, awsCredentials.AwsSecret);
 
+            var config = new AmazonS3Config()
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.EUNorth1
+            };
+
+            try
+            {
+                var uploadRequest = new TransferUtilityUploadRequest()
+                {
+                    InputStream = image.OpenReadStream(),
+                    Key = $"CategoriesPictures/{Guid.NewGuid()}{Path.GetExtension(image.FileName)}",
+                    BucketName = bucketName,
+                    CannedACL = S3CannedACL.NoACL
+                };
+
+                using var client = new AmazonS3Client(credentials, config);
+                var transferUtility = new TransferUtility(client);
+                await transferUtility.UploadAsync(uploadRequest);
+
+                return $"https://{bucketName}.s3.amazonaws.com/{uploadRequest.Key}";
+            }
+            catch (AmazonS3Exception ex)
+            {
+                throw new Exception($"Amazon S3 error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error uploading image: {ex.Message}");
+            }
+        }
     }
 }
