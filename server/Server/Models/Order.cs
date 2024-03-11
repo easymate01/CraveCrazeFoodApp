7e0bff296bc3dc65ced8ec14a57bb78354c2c9ad
Gulyas Mate <mategulyas01@gmail.com>
2024-03-11 21:19:36 +0100

    Customer, Order Models updated.

--- server/Server/Models/Order.cs
+++ server/Server/Models/Order.cs
@@ -1,8 +1,19 @@
-﻿namespace Server.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Server.Models
 {
     public class Order
     {
         public int Id { get; set; }
+
+        [JsonIgnore]
+        public Restaurant Restaurant { get; set; }
         public int RestaurantId { get; set; }
+
+        [JsonIgnore]
+        public Customer Customer { get; set; }
+        public int CustomerId { get; set; }
+
+        public List<Dish> Items { get; set; }
     }
 }
