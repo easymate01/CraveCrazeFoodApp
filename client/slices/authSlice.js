2b32e599945daf13a5130252d1d136731ae7784d
Gulyas Mate <mategulyas01@gmail.com>
2024-05-02 17:59:18 +0200

    authSlice added.

---
+++ client/slices/authSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+  loading: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+      state.loading = false;
+    },
+    loginFailure: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+      state.loading = false;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+  },
+});
+
+export const { loginSuccess, loginFailure, setLoading } = authSlice.actions;
+
+export const selectUser = (state) => state.auth.user;
+export const selectIsAuthenticated = (state) => state.auth.isAuthenticated;
+export const selectLoading = (state) => state.auth.loading;
+
+export default authSlice.reducer;
--- client/store.js
+++ client/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
 import resturantSlice from "./slices/restaurantSlice";
+import authSlice from "./slices/authSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice,
     restaurant: resturantSlice,
+    auth: authSlice,
   },
 });
