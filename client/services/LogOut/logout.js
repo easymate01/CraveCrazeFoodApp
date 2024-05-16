2a1c8bf0b6b93b16409ef45bdf2a2a5459ffb1bf
Gulyas Mate <mategulyas01@gmail.com>
2024-05-16 14:34:53 +0200

    Fix logout, to navigate logged out user to Starter screen.

--- client/services/LogOut/logout.js
+++ client/services/LogOut/logout.js
@@ -2,12 +2,18 @@ import { useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../slices/authSlice";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const useLogout = () => {
   const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return useCallback(() => {
     dispatch(logout());
     AsyncStorage.removeItem("userData");
+    navigation.navigate("Starter");
+    Alert.alert("You Logged out!");
   }, [dispatch]);
 };
 export default useLogout;
